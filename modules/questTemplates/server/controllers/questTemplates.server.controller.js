5ad80dff423fb1ad72caa971c7c363e40f185c81
Domines <719480072@qq.com>
2018-12-16 23:24:43 +0800

    add questTemplate and paperTemplate module

---
+++ modules/questTemplates/server/controllers/questTemplates.server.controller.js
@@ -0,0 +1,117 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+var path = require('path'),
+  mongoose = require('mongoose'),
+  QuestTemplate = mongoose.model('QuestTemplate'),
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
+
+/**
+ * Create an article
+ */
+exports.create = function (req, res) {
+  var questTemplate = new QuestTemplate(req.body);
+  questTemplate.user = req.user.id;
+
+  questTemplate.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(questTemplate);
+    }
+  });
+};
+
+/**
+ * Show the current article
+ */
+exports.read = function (req, res) {
+  // convert mongoose document to JSON
+  var questTemplate = req.questTemplate ? req.questTemplate.toJSON() : {};
+
+  // Add a custom field to the Article, for determining if the current User is the "owner".
+  // NOTE: This field is NOT persisted to the database, since it doesn't exist in the Article model.
+  questTemplate.isCurrentUserOwner = !!(req.user && questTemplate.user && questTemplate.user._id.toString() === req.user._id.toString());
+
+  res.json(questTemplate);
+};
+
+/**
+ * Update an article
+ */
+exports.update = function (req, res) {
+  var article = req.article;
+
+  article.title = req.body.title;
+  article.content = req.body.content;
+
+  article.save(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(article);
+    }
+  });
+};
+
+/**
+ * Delete an article
+ */
+exports.delete = function (req, res) {
+  var article = req.article;
+
+  article.remove(function (err) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(article);
+    }
+  });
+};
+
+/**
+ * List of Articles
+ */
+exports.list = function (req, res) {
+  QuestTemplate.find().sort('-created').populate('user', 'displayName').exec(function (err, articles) {
+    if (err) {
+      return res.status(422).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(articles);
+    }
+  });
+};
+
+/**
+ * Article middleware
+ */
+exports.articleByID = function (req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Article is invalid'
+    });
+  }
+
+  QuestTemplate.findById(id).populate('user', 'displayName').exec(function (err, article) {
+    if (err) {
+      return next(err);
+    } else if (!article) {
+      return res.status(404).send({
+        message: 'No article with that identifier has been found'
+      });
+    }
+    req.article = article;
+    next();
+  });
+};
