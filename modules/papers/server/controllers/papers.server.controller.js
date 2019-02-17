74d40d77ed8b4b7b7878472ac27d16b7a86f0fea
Domines <719480072@qq.com>
2019-02-17 17:09:03 +0800

    增加试卷查询逻辑的promise支持

--- modules/papers/server/controllers/papers.server.controller.js
+++ modules/papers/server/controllers/papers.server.controller.js
@@ -5,6 +5,7 @@
  */
 var path = require('path'),
   mongoose = require('mongoose'),
+  Promise = require('promise'),
   Paper = mongoose.model('Paper'),
   Singlechoice = mongoose.model('Singlechoice'),
   Multichoice = mongoose.model('Multichoice'),
@@ -17,95 +18,6 @@ var path = require('path'),
   _ = require('lodash');
 
 
-var querySingleChoice = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Singlechoice.find(query).limit(questsSet.number).exec(function(err, singlechoices) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = singlechoices;
-    }
-  });
-}
-
-var queryMultiChoice = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Multichoice.find(query).limit(questsSet.number).exec(function(err, multichoices) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = multichoices;
-    }
-  });
-}
-
-var queryBlank = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Blank.find(query).limit(questsSet.number).exec(function(err, blanks) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = blanks;
-    }
-  });
-}
-
-var queryeJudge = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Judge.find(query).limit(questsSet.number).exec(function(err, judges) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = judges;
-    }
-  });
-}
-
-var queryQuestAnswer = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Questanswer.find(query).limit(questsSet.number).exec(function(err, questanswers) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = questanswers;
-    }
-  });
-}
-
-var queryMixing = function(questsSet){
-  var query= {random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}};
-  var tags = questsSet.tags;
-  if (tags.length >  0){
-    query.tags = {"$all": tags};
-  }
-  Mixing.find(query).limit(questsSet.number).exec(function(err, mixings) {
-    if (err) {
-      console.log(err);
-    } else {
-      questsSet.questions = mixings;
-    }
-  });
-}
 /**
  * Create a Paper
  */
@@ -119,35 +31,56 @@ exports.create = function(req, res) {
   var paperStructs = template.paperStructs;
 
   var questions = [];
+  var queryPromises = [];
+
+  var subjectId = mongoose.Types.ObjectId(template.subject);
+  var query = {subject: subjectId,
+    random: {$near: [Math.random().toFixed(2), Math.random().toFixed(2)]}
+  };
+
   for(var index in paperStructs) {
     var questsSet = paperStructs[index];
 
+    var difficulty = parseFloat(questsSet.difficulty);
+    query.difficulty = {$gte: difficulty-1, $lte: difficulty+1};
+    var tags = questsSet.tags;
+    if (tags.length >  0){
+      query.tags = {"$all": tags};
+    }
+
     if (questsSet.questType === 'singleChoice'){
-      querySingleChoice(questsSet);
+      queryPromises.push(Singlechoice.find(query).limit(parseInt(questsSet.number)).exec());
     } else if (questsSet.questType === 'multiChoice'){
-      queryMultiChoice(questsSet);
+      queryPromises.push(Multichoice.find(query).limit(parseInt(questsSet.number)).exec());
     } else if (questsSet.questType === 'blank'){
-      queryBlank(questsSet);
+      queryPromises.push(Blank.find(query).limit(parseInt(questsSet.number)).exec());
     } else if (questsSet.questType === 'judge'){
-      queryeJudge(questsSet);
+      queryPromises.push(Judge.find(query).limit(parseInt(questsSet.number)).exec());
     } else if (questsSet.questType === 'questAnswer'){
-      queryQuestAnswer(questsSet);
+      queryPromises.push(Questanswer.find(query).limit(parseInt(questsSet.number)).exec());
     } else if (questsSet.questType === 'mixing'){
-      queryMixing(questsSet);
+      queryPromises.push(Mixing.find(query).limit(parseInt(questsSet.number)).exec());
     }
-
-    questions.push(questsSet);
   }
-  paper.questions = questions;
 
-  paper.save(function(err) {
-    if (err) {
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
-      });
-    } else {
-      res.jsonp(paper);
+  Promise.all(queryPromises).then(function(values) {
+    for(var index in paperStructs) {
+      var questsSet = paperStructs[index];
+      questsSet.questions = values[index];
+      questions.push(questsSet);
     }
+    paper.questions = questions;
+    paper.save(function(err) {
+      if (err) {
+        return res.status(400).send({
+          message: errorHandler.getErrorMessage(err)
+        });
+      } else {
+        res.jsonp(paper);
+      }
+    });
+  }).catch(function(reason) {
+    console.error(reason);
   });
 };
 
